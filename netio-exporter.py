f3cb9589f04a1283b2cd8a21883c7e7666eddfdb
Jan Tomsa <jan@tomsa.xyz>
2022-04-25 19:38:45 +0200

    improve comment

--- netio-exporter.py
+++ netio-exporter.py
@@ -132,7 +132,7 @@ class NetioCollector:
             'json_version': self.data.get('Agent', {}).get('JSONVer'),
             'name': self.data.get('Agent', {}).get('DeviceName'),
             'outputs': str(self.data.get('Agent', {}).get('NumOutputs')),
-            # in cobra, there is a `mac` field instead of the `SerialNumber` field
+            # in cobra, there is a `mac` field instead of the `SerialNumber` field. There are also some 4Cs, that have `SerialNumber` empty.
             'sn': (self.data.get('Agent', {}).get('SerialNumber') or
                    self.data.get('Agent', {}).get('MAC')) or
                   'Unknown',
